b9f44d49f7e159f600d12fda97ef0e1801894d8f
Adib Hasan <87318124+myselfadib@users.noreply.github.com>
2022-06-11 20:06:57 +0600

    upto delete function

---
+++ basket/views.py
@@ -0,0 +1,36 @@
+
+from urllib import response
+from django.shortcuts import render, get_object_or_404
+from store.models import Product
+from .basket import Basket
+from django.http import JsonResponse
+# Create your views here.
+
+
+def basket_summary(request):
+    basket = Basket(request)
+    return render(request, 'store/basket/summary.html', {'basket': basket})
+
+
+def basket_add(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
+        product = get_object_or_404(Product, id=product_id)
+        basketqty = basket.__len__()
+        basket.add(product=product, qty=product_qty)
+        response = JsonResponse({'qty': basketqty})
+        return response
+
+
+def basket_delete(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        basket.delete(product=product_id)
+        
+        basketqty = basket.__len__()
+        baskettotal = basket.get_total_price()
+        response = JsonResponse({'qty': basketqty, 'subtotal': baskettotal})
+        return response
