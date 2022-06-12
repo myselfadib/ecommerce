64467917e896f2f2a70aa1200833d91c4445f6bf
Adib Hasan <87318124+myselfadib@users.noreply.github.com>
2022-06-12 11:43:12 +0600

    update funtion applied

--- basket/basket.py
+++ basket/basket.py
@@ -67,7 +67,14 @@ class Basket():
             del self.basket[product_id]
             self.save()
         
-            
+    def update(self,product,qty):
+        """
+        update values in session data
+        """
+        product_id = str(product)
+        if product_id in self.basket:
+            self.basket[product_id]['qty']=qty
+        self.save()
 
     def save(self):
         self.session.modified = True
