c02c14136575c9514139a7fd545e7771db0beb78
Adib Hasan <87318124+myselfadib@users.noreply.github.com>
2022-06-17 19:58:29 +0600

    registration process

--- account/views.py
+++ account/views.py
@@ -1,5 +1,20 @@
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.sites.shortcuts import get_current_site
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
-from .forms import RegistrationForm
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+
+#from orders.views import user_orders
+
+from .forms import RegistrationForm, UserEditForm
+from .models import UserBase
+#from .tokens import account_activation_token
+
+
+@login_required
 # Create your views here.
 
 def account_register(request):
@@ -15,6 +30,16 @@ def account_register(request):
             user.save()
 
             # setup email
+            current_site = get_current_site(request)
+            subject = 'Activate your account'
+            message = render_to_string('account/registration/account_activation_email.html',{
+                'user': user,
+                'domain': current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            user.email_user(subject=subject,message=message)
+
             
 
 
