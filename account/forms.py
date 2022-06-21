4c075bae01e6f8e4f884389e9f0edb6b393246d9
Adib Hasan <87318124+myselfadib@users.noreply.github.com>
2022-06-21 17:29:52 +0600

    updated login system

--- account/forms.py
+++ account/forms.py
@@ -12,6 +12,39 @@ class RegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
     password2 = forms.CharField(
         label='Repeat password', widget=forms.PasswordInput)
+
     class Meta:
         model = UserBase
-        fields = ('user_name', 'email',)    
+        fields = ('user_name', 'email',)
+
+    def clean_username(self):
+        user_name = self.cleaned_data['user_name'].lower()
+        r = UserBase.objects.filter(user_name=user_name)
+        if r.count():
+            raise forms.ValidationError('Username already exit')
+        return user_name
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords do not match')
+        return cd['password2']
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        r = UserBase.objects.filter(email=email)
+        if r.exists():
+            raise forms.ValidationError(
+                'Please use another email, that is already taken')
+        return email
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['user_name'].widget.attrs.update(
+            {'class': 'form-control mb-3', 'placeholder': 'Username'})
+        self.fields['email'].widget.attrs.update(
+            {'class': 'form-control mb-3', 'placeholder': 'E-mail', 'name': 'email', 'id': 'id_email'})
+        self.fields['password'].widget.attrs.update(
+            {'class': 'form-control mb-3', 'placeholder': 'Password'})
+        self.fields['password2'].widget.attrs.update(
+            {'class': 'form-control', 'placeholder': 'Repeat Password'})
