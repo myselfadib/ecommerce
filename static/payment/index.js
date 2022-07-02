852099e15decdc73a8de169e824bf3c69b030401
Adib Hasan <adibhasan50@gmail.com>
2022-07-02 20:05:03 +0600

    updated

--- static/payment/index.js
+++ static/payment/index.js
@@ -1,4 +1,4 @@
-var stripe = stripe('sk_test_51LGNYHK9Mq6UmztsyW63k9TTi4nbfBzoew7qsMxkgFRcfaiKvSs5NMsr6K6U8PKwx4BtH5TI39uxcY3C24J9HQDL004yG4wz95');
+var stripe = Stripe('sk_test_51LGNYHK9Mq6UmztsyW63k9TTi4nbfBzoew7qsMxkgFRcfaiKvSs5NMsr6K6U8PKwx4BtH5TI39uxcY3C24J9HQDL004yG4wz95');
 
 var elm = document.getElementById('submit');
 clientsecret = elm.getAttribute('data-secret');
@@ -37,6 +37,49 @@ form.addEventListener('submit', function(ev)
   var custAdd = document.getElementById("custAdd").value;
   var custAdd2 = document.getElementById("custAdd2").value;
   var postCode = document.getElementById("postCode").value;
+
+  $.ajax({
+    type: "POST",
+    url: 'http://127.0.0.1:8000/orders/add/',
+    data: {
+      order_key: clientsecret,
+      csrfmiddlewaretoken: CSRF_TOKEN,
+      action: "post",
+    },
+    success: function (json) {
+      console.log(json.success)
+
+      stripe.confirmCardPayment(clientsecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            address:{
+                line1:custAdd,
+                line2:custAdd2
+            },
+            name: custName
+          },
+        }
+      }).then(function(result) {
+        if (result.error) {
+          console.log('payment error')
+          console.log(result.error.message);
+        } else {
+          if (result.paymentIntent.status === 'succeeded') {
+            console.log('payment processed')
+            // There's a risk of the customer closing the window before callback
+            // execution. Set up a webhook or plugin to listen for the
+            // payment_intent.succeeded event that handles any business critical
+            // post-payment actions.
+            window.location.replace("http://127.0.0.1:8000/payment/orderplaced/");
+          }
+        }
+      });
+
+    },
+    error: function (xhr, errmsg, err) {},
+  });
+
 
   
 
